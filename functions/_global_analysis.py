08b156dd533604b1b20c6b30d8a8d3a66ef6d150
mlempp <martin@lempp.online>
2022-04-06 18:02:47 +0200

    20220406-1: made updates on the global analysis

--- functions/_global_analysis.py
+++ functions/_global_analysis.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import os
 from tqdm import tqdm
+from collections import Counter
 path = os.getcwd() + '/'
 from _video_class import *
+
 
 def _global_analysis(path):
 
@@ -17,9 +19,21 @@ def _global_analysis(path):
     for id in tqdm(video_ids_OI):
         tmp_video = video_df.loc[id]
         tmp_comment = comment_df.loc[id]
-        tmp_class = video(id, tmp_video.video_title)
+        tmp_class = video(id, tmp_video.video_title, tmp_video.video_date)
         tmp_class.add_comments(tmp_comment.comments)
+        tmp_class.prep_comments()
         full_comment_list = full_comment_list+tmp_class.comments_list
         video_dct[id] = tmp_class
+
+
+    all_words_list = [x.split() for x in full_comment_list]
+    all_words_flat = [ item for elem in all_words_list for item in elem]
+    count_dict = dict((x, all_words_flat.count(x)) for x in set(all_words_flat))
+
+
+
+
+    #most frequent words
+    #sentiment per week
 
     pass
--- functions/_video_class.py
+++ functions/_video_class.py
@@ -2,6 +2,16 @@ class video:
     def __init__(self, video_id, video_title):
         self.video_id = video_id
         self.video_title = video_title
+        self.video_date = video_date
+
 
     def add_comments(self, comments_list):
         self.comments_list = comments_list.split(' || ')
+
+    def prep_comments(self):
+        # 1. spell check
+        # 2. correct abbreviate words
+        # 1. spell check
+        # 1. spell check
+        # 1. spell check
+        # 1. spell check
