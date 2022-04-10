7575ff811654a64b3f6b4d81fa60725321c55cfb
mlempp <martin@lempp.online>
2022-04-10 16:31:30 +0200

    20220410-1: downloaded the first sentiment dict

--- functions/_defined_past_analysis.py
+++ functions/_defined_past_analysis.py
@@ -17,6 +17,21 @@ def defined_past_analysis(delta,outpath):
     video_df_red = video_df[video_df.video_date > limit]
     video_ids_OI = list(video_df_red.index)
 
+    comment_df = pd.read_csv(path+'comment_DataBase.csv', sep = ';', index_col = 0)
+
+    video_dct = {}
+    full_comment_list =[]
+    full_preped_comment_list =[]
+    for id in tqdm(video_ids_OI):
+        tmp_video = video_df.loc[id]
+        tmp_comment = comment_df.loc[id]
+        tmp_class = video(id, tmp_video.video_title, tmp_video.video_date)
+        tmp_class.add_comments(tmp_comment.comments)
+        tmp_class.prep_comments()
+        full_comment_list = full_comment_list+tmp_class.comments_list
+        full_preped_comment_list = full_preped_comment_list+tmp_class.comments_list_preped
+        video_dct[id] = tmp_class
+
 
 
     pass
--- functions/_video_class.py
+++ functions/_video_class.py
@@ -1,12 +1,20 @@
 path = os.getcwd() + '/'
 sys.path.insert(0, path + 'functions/')
 from _helper_functions import *
+import pandas as pd
 
 class video:
     def __init__(self, video_id, video_title, video_date):
         self.video_id = video_id
         self.video_title = video_title
         self.video_date = video_date
+        senti_ws_positive = pd.read_csv(path + 'functions/SentiWS_v1.8c_Positive.txt', sep = '\t', header = None)
+        senti_ws_negative = pd.read_csv(path + 'functions/SentiWS_v1.8c_Negative.txt', sep = '\t', header = None)
+        senti_ws = pd.concat([senti_ws_positive, senti_ws_negative], axis=0, ignore_index=True)
+        senti_ws[0] = senti_ws[0].apply(lambda x: x.split('|')[0])
+        senti_ws = senti_ws.set_index(0)
+        senti_ws = senti_ws[1].to_dict()
+        self.senti_ws = pd.concat([senti_ws_positive, senti_ws_negative], axis = 0,ignore_index=True)
 
 
     def add_comments(self, comments_list):
@@ -18,3 +26,6 @@ class video:
         for comment in comments_list:
             comments_list_preped.append(clean_text(comment))
         self.comments_list_preped = comments_list_preped
+
+    def calc_sentiment_score1(self, txt):
+        compare_strings = self.senti_ws.keys()
